b06c9c9f71ed87dc1bec23a222f607c6c845bcf0
Satar Sarafa <sp3c7r40x00@gmail.com>
2025-04-29 15:20:29 +0100

    icare server changes

--- app/server.ts
+++ app/server.ts
@@ -1,4 +1,4 @@
-import express, { type Request, type Response } from 'express';
+import express from 'express';
 import Logger from '../src/utils/logger';
 import { address } from 'ip';
 import env from '../src/config/env';
@@ -7,7 +7,6 @@ import errorHandler from '../src/middlewares/errorMiddleWare';
 import userRoutes from './../src/routes/user.routes';
 import startSocketServer from './socket';
 import { createServer } from 'http';
-import { cacheGet } from '../src/config/redis';
 
 // redis.connect()
 const app = express();
@@ -24,13 +23,6 @@ connectNoSQL(); /* Connect to MongDB */
 
 /* User Routes */
 app.use('/api/v1/user/', userRoutes);
-
-app.get('/get', async (req: Request, res: Response) => {
-  const cachedData = await cacheGet('movies');
-  console.log(cachedData);
-  if (cachedData) return res.status(200).json(JSON.parse(cachedData)); // Cache hit
-  res.status(201).json(env);
-});
 
 /** Handles Error */
 app.use(errorHandler);
--- app/socket.ts
+++ app/socket.ts
@@ -4,7 +4,37 @@ import Logger from '../src/utils/logger';
 import fs from 'fs';
 import path from 'path';
 import { createDirectory } from '../src/utils/utility';
-import AwsTools from '../src/utils/awsTools';
+import AwsTools from '../src/utils/AwsTools';
+
+async function uploadAudio(data, callback) {
+  const { file, fileName } = data;
+
+  if (!file || !fileName) {
+    callback({ success: false, error: 'Invalid file or fileName' });
+    return;
+  }
+
+  // Decode Base64 and save the file
+  const recordingsDir = createDirectory('recordings');
+  const filePath = path.join(recordingsDir, fileName);
+  const buffer = Buffer.from(file, 'base64');
+
+  fs.writeFile(filePath, buffer, (err) => {
+    if (err) {
+      console.error('Error saving file:', err);
+      callback({ success: false, error: 'Failed to save file' });
+    } else {
+      console.log('File saved successfully:', filePath);
+      callback({ success: true });
+    }
+  });
+  const transcribe = await AwsTools.audioTranscribe2(filePath);
+  console.log(
+    'Transcribe: ',
+    transcribe?.results.channels[0].alternatives[0].transcript,
+  );
+  return transcribe?.results.channels[0].alternatives[0].transcript;
+}
 
 const startSocketServer = (server: HttpServer) => {
   // const audioChunks: string[] = []
@@ -20,32 +50,8 @@ const startSocketServer = (server: HttpServer) => {
     console.log('Client connected');
 
     socket.on('upload', async (data, callback) => {
-      const { file, fileName } = data;
-
-      if (!file || !fileName) {
-        callback({ success: false, error: 'Invalid file or fileName' });
-        return;
-      }
-
-      // Decode Base64 and save the file
-      const recordingsDir = createDirectory('recordings');
-      const filePath = path.join(recordingsDir, fileName);
-      const buffer = Buffer.from(file, 'base64');
-
-      fs.writeFile(filePath, buffer, (err) => {
-        if (err) {
-          console.error('Error saving file:', err);
-          callback({ success: false, error: 'Failed to save file' });
-        } else {
-          console.log('File saved successfully:', filePath);
-          callback({ success: true });
-        }
-      });
-      const transcribe = await AwsTools.audioTranscribe2(filePath);
-      console.log(
-        'Transcribe: ',
-        transcribe?.results.channels[0].alternatives[0].transcript,
-      );
+      const response = uploadAudio(data, callback);
+      console.log(response);
     });
 
     socket.on('disconnect', () => {
